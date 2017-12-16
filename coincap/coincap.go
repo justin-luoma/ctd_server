10b3de2f3967108cd916746dbb1079cef253ca80
MrSpicyWeiner <MrSpicyWeiner365@gmail.com>
2017-12-16 14:49:11 -0600

    Added get_map function to get list of coins Added get_page function to get information about a specific coin Need to make GetCoinCapCoin convert `page` into a Coin object still.

--- coincap/coincap.go
+++ coincap/coincap.go
@@ -1,38 +1,71 @@
 package coincap
 
 import (
+	"../restful_query"
 	"encoding/json"
-	"net/http"
-	"fmt"
-	"time"
 	"log"
+	"math/big"
+	"time"
+	"github.com/golang/glog"
+	"flag"
 )
 
-var apiUrl string = "https://coincap.io"
+func init()  {
+	flag.Parse()
+}
+
+var apiUrl string = "https://coincap.io/"
+
+type CoinCapMap struct {
+	Aliases []interface{} `json:"aliases"`
+	Name    string        `json:"name,omitempty"`
+	Symbol  string        `json:"symbol,omitempty"`
+}
 
 type CoinCapPage struct {
-	ID             string  `json:"id"`
+	Id             string  `json:"id"`
 	DisplayName    string  `json:"display_name"`
 	Cap24HrChange  float64 `json:"cap24hrChange"`
 	PriceBtc       float64 `json:"price_btc"`
-	PriceEur       float64 `json:"price_eur"`
+	PriceEth       float64 `json:"price_eth"`
 	PriceUsd       float64 `json:"price_usd"`
 	QueryTimeStamp int64   `json:"query_timestamp"`
 }
 
-func GetCoinCapCoin(id string) *CoinCapPage {
-	coin := CoinCapPage{QueryTimeStamp: time.Now().Unix()}
+type Coin struct {
+	Id             string    `json:"id"`
+	DisplayName    string    `json:"display_name"`
+	PriceBtc       big.Float `json:"price_btc"`
+	PriceEth       big.Float `json:"price_eth"`
+	PriceUsd       big.Float `json:"price_usd"`
+	QueryTimeStamp int64     `json:"query_timestamp"`
+}
 
-	url := fmt.Sprintf("%s/page/%s",apiUrl, id)
-	response, err := http.Get(url)
+func get_map() ([]CoinCapMap, error) {
+	bodyBytes, err := restful_query.Get(apiUrl + "map")
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		err := json.NewDecoder(response.Body).Decode(&coin)
-		if err != nil {
-			log.Println(err)
-		}
-		//fmt.Println(coin)
+		log.Fatalln(err)
 	}
-	return &coin
+	var coinMap []CoinCapMap
+	json.Unmarshal(bodyBytes, &coinMap)
+
+	return coinMap, nil
+}
+
+func get_page(product string) CoinCapPage {
+	glog.V(2).Infoln("Testing")
+	bodyBytes, err := restful_query.Get(apiUrl + "page/" + product)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	page := CoinCapPage{Id: product,
+		QueryTimeStamp: time.Now().Unix()}
+	json.Unmarshal(bodyBytes, &page)
+	return page
+}
+
+func GetCoinCapCoin(id string) *CoinCapPage {
+	page := get_page(id)
+
+	return &page
 }
