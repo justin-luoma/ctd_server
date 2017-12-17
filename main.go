642f850e875769eaa8aaa6947d21dc950dc53206
Justin <caboose0013@gmail.com>
2017-12-17 15:29:29 -0600

    removed gdax.Init() from main as it is not longer needed

--- main.go
+++ main.go
@@ -42,7 +42,6 @@ type Address struct {
 
 // our main function
 func main() {
-	gdax.Init()
 	exchange_api_status.Start_Exchange_Monitoring()
 	/*people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
 	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
